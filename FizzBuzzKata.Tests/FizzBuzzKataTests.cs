2d6b5e8e3a8412e81673540abae8a9da511ba6f3
Ivy <ivy.cm.wong@gmail.com>
2015-11-08 23:39:00 -0800

    Added unit tests.

---
+++ FizzBuzzKata.Tests/FizzBuzzKataTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzzKata.Tests
+{
+    [TestFixture]
+    public class FizzBuzzKataTests
+    {
+        private Game game;
+        private string result;
+
+        [TestFixtureSetUp]
+        public void CreateGame()
+        {
+            game = new Game();
+            result = "";
+        }
+
+        [Test]
+        public void ShouldReturnOne()
+        {
+            game.SetRange(1);
+            result = game.Print();
+            Assert.That(result, Is.EqualTo("1, "));
+        }
+
+        [Test]
+        public void ShouldReturnThreeAsFizz()
+        {
+            game.SetRange(3);
+            result = game.Print();
+            Assert.That(result, Is.EqualTo("1, 2, Fizz, "));
+        }
+
+        [Test]
+        public void ShouldReturnFiveAsBuzz()
+        {
+            game.SetRange(5);
+            result = game.Print();
+            Assert.That(result, Is.EqualTo("1, 2, Fizz, 4, Buzz, "));
+        }
+
+        //[Test]
+        public void ShouldReturnFifteenAsFizzBuzz()
+        {
+            game.SetRange(15);
+            result = game.Print();
+
+        }
+    }
+}
---
+++ FizzBuzzKata/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzKata
+{
+    public class Game
+    {
+        private int range;
+
+        public Game()
+        {
+            range = 0;
+        }
+
+        public void SetRange(int value)
+        {
+            range = value;
+        }
+
+        public string Print()
+        {
+            var result = "";
+            for (int number = 1; number <= range; number++)
+            {
+                if (isDivisibleByThree(number))
+                {
+                    result += "Fizz, ";
+                }
+                else
+                {
+                    result += number + ", ";
+                }
+            }
+            return result;
+        }
+
+        private bool isDivisibleByThree(int value)
+        {
+            return value%3 == 0;
+        }
+    }
+}
